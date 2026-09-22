c6e46fecb4b2c0673d55dfef799611869c84490a
Everett Lopez <everettlopezsr@gmail.com>
2026-09-22 17:04:25 -0600

    Created assignment 2. Added basic screen files, and navgation.

---
+++ assignment_2/app/src/main/java/com/example/assignment2/ui/screens/CourseDetailScreen.kt
@@ -0,0 +1,10 @@
+package com.example.assignment2
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CourseDetailScreen()
+{
+    Text("Course Detail Screen")
+}
